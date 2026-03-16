e41d20a7f6076b9ebcbcbe2c6a5bcd04e5e768e6
Vika <vikababyuk15@gmail.com>
2026-03-16 14:43:22 +0200

    1version

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const cards = document.querySelectorAll('.card');
+
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            card.classList.toggle('is-flipped');
+        });
+    });
+});
